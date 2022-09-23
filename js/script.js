ba7346aaeac24cd8f6317dcd17d849e4bd1d4eb1
Fennyjong535210001 <fenny.535210001@stu.untar.ac.id>
2022-09-23 22:57:02 +0700

    button add

--- js/script.js
+++ js/script.js
@@ -11,3 +11,18 @@ $('.log-in').click((event)=>{
   console.log("A");
   window.location.href="../html/signin.html";
 });
+
+/* add button micro */
+const btnEl = document.querySelector('.btn');
+const iconEl = document.querySelector('.icons');
+const toggleOptions = () => {
+    const wrapper = document.querySelector('.wrapper');
+    console.log(iconEl);
+    wrapper.classList.toggle('active');
+    if (iconEl.classList.contains('fa-share-alt')) {
+        iconEl.classList.replace('fa-share-alt','fa-close');
+    } else {
+        iconEl.classList.replace('fa-close', 'fa-share-alt');
+    }
+};
+btnEl.addEventListener('click', toggleOptions);
