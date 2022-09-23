5024b0193c71431ca3afa90c03cbc42c7ab8fd8b
Valerie-0036 <valerie.535210036@stu.untar.ac.id>
2022-09-23 21:35:43 +0700

    tidy

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$('#comment-button').click((event)=>{
+  $('.comment').toggle();
+});
+
+function redirectLogin(){
+
+console.log("A");
+};
+
+$('.log-in').click((event)=>{
+  console.log("A");
+  window.location.href="../html/signin.html";
+});
