5bcdaa76c8db91e91c4181e114d3f832e38c75b5
Vachell <vastiansen@gmail.com>
2022-09-26 10:35:39 +0700

    Comment Section

---
+++ sandbox/js/index.js
@@ -0,0 +1,15 @@
+function comment(count) {
+  var bubbles = document.getElementsByClassName("interactive-bubble")[count];
+  var music = document.getElementsByClassName("music")[count];
+  var userCaption = document.getElementsByClassName("user-caption")[count];
+
+  userCaption.classList.toggle("hidden");
+  music.classList.toggle("hidden");
+  bubbles.classList.toggle("small");
+
+  var comment = document.getElementsByClassName("comment-container")[count];
+  var captionComment = document.getElementsByClassName("caption-comment")[count];
+
+  comment.classList.toggle("hidden");
+  captionComment.classList.toggle("show-comments");
+;}
