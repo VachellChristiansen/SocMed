431033c1983cfc69099a3dfc93857b562541b1ef
AndrewLimm <andrewlim389@gmail.com>
2022-09-27 22:19:48 +0700

    user page

--- js/user.js
+++ js/user.js
@@ -8,3 +8,22 @@ $(".content-video").mouseout(function(){
 function redirectUser(profile) {
   location.replace(`profiles/${profile}.html`);
 }
+
+//back to top
+// Get the button:
+let buttonTop = document.getElementById("button-top");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    buttonTop.style.display = "block";
+  } else {
+    buttonTop.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; 
+}
