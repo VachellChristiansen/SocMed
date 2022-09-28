dd2e6d78f0a8d73c90f4775873aaf5a96a299fc7
Naramia <naramia.535210018@stu.untar.ac.id>
2022-09-28 11:38:36 +0700

    update upload

--- js/upload.js
+++ js/upload.js
@@ -1,69 +1,72 @@
-document.querySelectorAll(".drop-zone__input").forEach(inputElement => {
-    const dropZoneElement =  inputElement.closest(".drop-zone");
-
-    dropZoneElement.addEventListener("click", e => {
-        inputElement.click();
+document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
+    const dropZoneElement = inputElement.closest(".drop-zone");
+  
+    dropZoneElement.addEventListener("click", (e) => {
+      inputElement.click();
     });
-
-    inputElement.addEventListener("change", e => {
-        if (inputElement.files.length) {
-            updateThumbnail(dropZoneElement, inputElement.files[0]);
-        };
-    })
-
-    dropZoneElement.addEventListener("dragover", e => {
-        e.preventDefault();
-        dropZoneElement.classList.add("drop-zone--over");
+  
+    inputElement.addEventListener("change", (e) => {
+      if (inputElement.files.length) {
+        updateThumbnail(dropZoneElement, inputElement.files[0]);
+      }
     });
-
-    ["dragleave", "dragend"].forEach(type => {
-        dropZoneElement.addEventListener(type, e => {
-            dropZoneElement.classList.remove("drop-zone--over");
-        });
+  
+    dropZoneElement.addEventListener("dragover", (e) => {
+      e.preventDefault();
+      dropZoneElement.classList.add("drop-zone--over");
     });
-
-    dropZoneElement.addEventListener("drop", e => {
-        e.preventDefault();
-
-        if (e.dataTransfer.files.length) {
-            inputElement.files = e.dataTransfer.files;
-            updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
-        }
-
+  
+    ["dragleave", "dragend"].forEach((type) => {
+      dropZoneElement.addEventListener(type, (e) => {
         dropZoneElement.classList.remove("drop-zone--over");
+      });
     });
-});
-
-function updateThumbnail(dropZoneElement, file) {
+  
+    dropZoneElement.addEventListener("drop", (e) => {
+      e.preventDefault();
+  
+      if (e.dataTransfer.files.length) {
+        inputElement.files = e.dataTransfer.files;
+        updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+      }
+  
+      dropZoneElement.classList.remove("drop-zone--over");
+    });
+  });
+  
+  /**
+   * Updates the thumbnail on a drop zone element.
+   *
+   * @param {HTMLElement} dropZoneElement
+   * @param {File} file
+   */
+  function updateThumbnail(dropZoneElement, file) {
     let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
-
-    console.log(file);
-
-
-    // first time - remove the prompt
-    if (dropZoneElement.querySelector(".drop-zone__prompt")){
-        dropZoneElement.querySelector(".drop-zone__prompt")
+  
+    // First time - remove the prompt
+    if (dropZoneElement.querySelector(".drop-zone__prompt")) {
+      dropZoneElement.querySelector(".drop-zone__prompt").remove();
     }
-
-    // firts time - there is no thumbnail
+  
+    // First time - there is no thumbnail element, so lets create it
     if (!thumbnailElement) {
-        thumbnailElement = document.createElement("div");
-        thumbnailElement.classList.add("drop-zone__thumb");
-        dropZoneElement.appendChild(thumbnailElement);
+      thumbnailElement = document.createElement("div");
+      thumbnailElement.classList.add("drop-zone__thumb");
+      dropZoneElement.appendChild(thumbnailElement);
     }
-
+  
     thumbnailElement.dataset.label = file.name;
-
-    //show thumbnail for image file
+  
+    // Show thumbnail for image files
     if (file.type.startsWith("image/")) {
-        const reader = new FileReader();
-
-        reader.readAsDataURL(file);
-        reader.onnload = () => {
-            thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
-        };
-
+      const reader = new FileReader();
+  
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
+      };
     } else {
-        thumbnailElement.style.backgroundImage = null;
+      thumbnailElement.style.backgroundImage = null;
     }
-}
+  }
+  
