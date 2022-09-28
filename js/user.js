76024adfe30a08177eeed6404e1d63f194890118
Naramia <naramia.535210018@stu.untar.ac.id>
2022-09-28 21:52:43 +0700

    remove redundant user-follow

--- js/user.js
+++ js/user.js
@@ -43,32 +43,32 @@ function topFunction() {
 }
 
 
-$('.user-follow').click(function(event) {
-  if($(this).hasClass("follow")){
-    $(this).removeClass("follow").addClass("followed");
-    $(this).text("Followed");
+// $('.user-follow').click(function(event) {
+//   if($(this).hasClass("follow")){
+//     $(this).removeClass("follow").addClass("followed");
+//     $(this).text("Followed");
    
-  } else {
-    $(this).removeClass("followed unfollow").addClass("follow");
-    $(this).text("Follow");
+//   } else {
+//     $(this).removeClass("followed unfollow").addClass("follow");
+//     $(this).text("Follow");
   
-  }
-  });
+//   }
+//   });
   
-  $('.user-follow').mouseenter(function(event) {
-     if($(this).hasClass("followed")){
-       $(this).text("Unfollow");
-      $(this).addClass("unfollow");
-     } 
-    });
+//   $('.user-follow').mouseenter(function(event) {
+//      if($(this).hasClass("followed")){
+//        $(this).text("Unfollow");
+//       $(this).addClass("unfollow");
+//      } 
+//     });
   
   
-    $('.user-follow').mouseout(function(event) {
-      if($(this).hasClass("unfollow")){
-        $(this).text("Followed");
-        $(this).removeClass("unfollow");
-      }
-      });
+//     $('.user-follow').mouseout(function(event) {
+//       if($(this).hasClass("unfollow")){
+//         $(this).text("Followed");
+//         $(this).removeClass("unfollow");
+//       }
+//       });
   
   $('.user-follow').click(function(event) {
   if($(this).hasClass("follow")){
