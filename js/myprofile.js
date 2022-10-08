729adb199b00b27da406a7a0f3c4004c0f25c464
Valerie-0036 <valerie.535210036@stu.untar.ac.id>
2022-10-08 23:52:01 +0700

    myprofile, user1, adjustments

--- js/myprofile.js
+++ js/myprofile.js
@@ -28,3 +28,17 @@ $(".content-video").mouseout(function(){
   this.pause();
 });
 
+const fileinput=document.getElementById("file-input")
+const fileinputbtn=document.getElementById("file-input-btn")
+const image=document.getElementById("pp")
+
+fileinputbtn.addEventListener("click",() => {
+    fileinput.click()
+})
+
+fileinput.addEventListener("change",() => {
+    const file=fileinput.files[0]
+    if (!file)return
+    const url=URL.createObjectURL(file)
+    image.src=url
+})
