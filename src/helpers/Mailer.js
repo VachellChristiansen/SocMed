d85cc646283672f3c44776473e8db20a8ad3f367
Valerie-0036 <valerie.535210036@stu.untar.ac.id>
2022-12-01 17:41:44 +0700

    footer for email

--- src/helpers/Mailer.js
+++ src/helpers/Mailer.js
@@ -9,7 +9,7 @@ const mail = async (id, name, email, code) => {
       'sender' : {'email':'deepspace.office@gmail.com', 'name':'Sendinblue'},
       'replyTo' : {'email':'deepspace.office@gmail.com', 'name':'Sendinblue'},
       'to' : [{'name': name, 'email': email}],
-      'htmlContent' : '<html><body><h1>Click the button below to verify your email.</h1><br/><a href="deepspace-testing.up.railway.app/user/verifyEmail?code={{code}}&id={{id}}></a></body></html>',
+      'htmlContent' : '<html><body><h1>Hello! Welcome to DeepSpace! Please click the button down below to verify your account! </h1><br/><a href="deepspace-testing.up.railway.app/user/verifyEmail?code={{code}}&id={{id}}></a><br/> <p>You are receiving this email because you have visited our site or asked us about the regular newsletter. Make sure our messages get to your Inbox (and not your bulk or junk folders).<br><a target="_blank" href="https://deepspace-testing.up.railway.app/privacypolicy">Privacy policy</a></p><br/><a target="_blank" href="https://deepspace-testing.up.railway.app/"><img src="https://yrlnng.stripocdn.email/content/guids/6aafc857-4f28-47b0-b93a-c527cb0a8d5f/images/favicon.png" alt width="125" style="display: block;"></a></body></html>',
       'params' : {'code': code , 'id': id}
     }
   ).then(function(data) {
