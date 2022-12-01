0f9317a8ba5f5c0ded5db8c9088dbd928955ab48
Vachell <vastiansen@gmail.com>
2022-12-01 10:44:34 +0700

    adjustments

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -46,17 +46,18 @@ const error = async (req, res, next) => {
   return res.render("errorPage")
 }
 const shared = async (req, res, next) => {
+  const current = req.user || '';
   const videos = await Posts.find({}).exec();
   const users = await Users.find({}).exec();
   const top = await Users.find({}).sort({ followers: -1 }).limit(3).exec();
-  const recommended = await Users.find({ 'followers.username': { $ne: req.user.username } }).limit(3).exec();
+  const recommended = await Users.find({ 'followers.username': { $ne: current.username || '' } }).limit(3).exec();
   const user = [];
   // helper code to find whose video are posted, this will be based on the video being called
   await videos.forEach((video, index) => {
     user.push(users.find(user => user.id === video.userId))
   })
-  const current = req.user || '';
   return res.render("shared", { videos, users, user, top, recommended, current: current})
 }
+
 
 module.exports = { getIndex, followFromIndex, unfollowFromIndex, privacyPolicy, search, upload, error, shared }
