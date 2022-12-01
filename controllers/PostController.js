245fa097d6d533d92e0903fa43c5e41d8a9fbd09
Vachell <vastiansen@gmail.com>
2022-12-01 12:20:48 +0700

    post redirect after comment

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -6,9 +6,13 @@ const postComment = async (req, res, next) => {
   if(!req.user) {
     return res.redirect('/user/login');
   }
+  const from = req.query.from
   const post = await Posts.findById(req.query.postId).exec();
   post.comments.push({userId: req.user.id, image: req.user.image, username: req.user.username, comment: req.body.comment })
   post.save();
+  if(from == 'post') {
+    return res.redirect('/post/' + req.query.postId)
+  }
   return res.redirect('/')
 }
 
