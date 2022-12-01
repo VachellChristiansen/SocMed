d2c9ec0096be4683a1996b369cd17ecf8a8a337e
Vachell <vastiansen@gmail.com>
2022-12-01 17:09:09 +0700

    boilerplate Mailer

--- src/helpers/Mailer.js
+++ src/helpers/Mailer.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 const SibApiV3Sdk = require('sib-api-v3-sdk');
 SibApiV3Sdk.ApiClient.instance.authentications['api-key'].apiKey = process.env.SIB_API_KEY;
 
-const mail = async (user) => {
+const mail = async (name, email, code) => {
   new SibApiV3Sdk.TransactionalEmailsApi().sendTransacEmail(
     {
       'subject':'Hello from the Node SDK!',
       'sender' : {'email':'deepspace.office@gmail.com', 'name':'Sendinblue'},
       'replyTo' : {'email':'deepspace.office@gmail.com', 'name':'Sendinblue'},
-      'to' : [{'name': user.username, 'email': user.email}],
+      'to' : [{'name': name, 'email': email}],
       'htmlContent' : '<html><body><h1>This is a transactional email {{params.bodyMessage}}</h1></body></html>',
       'params' : {'bodyMessage':'Made just for you!'}
     }
