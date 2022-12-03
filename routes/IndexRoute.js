cc95ec73858c51bde3fdd51ad7c2075efeda9132
Fennyjong535210001 <fenny.535210001@stu.untar.ac.id>
2022-12-04 00:59:36 +0700

    route

--- routes/IndexRoute.js
+++ routes/IndexRoute.js
@@ -6,3 +6,16 @@ const { getIndex, followFromIndex, unfollowFromIndex, likeFromIndex, unlikeFromI
 const { check } = require(path.join(__dirname, "../middlewares/AccountCheck"));
 
 // route here starts with "localhost/"
+
+router.get("/", getIndex);
+router.get("/follow", check, followFromIndex);
+router.get("/unfollow", check, unfollowFromIndex);
+router.get("/like", check, likeFromIndex);
+router.get("/unlike", check, unlikeFromIndex);
+router.get("/privacypolicy", privacyPolicy);
+router.get("/search", search);
+router.get("/upload", check, upload);
+router.get("/error", error);
+router.get("/post/:postId", post);
+
+module.exports = router;
