f314b69bf17ca6120c94441dbc7b1bd106c71efb
Vachell <vastiansen@gmail.com>
2022-12-03 15:38:36 +0700

    self follow fix

--- middlewares/AccountCheck.js
+++ middlewares/AccountCheck.js
@@ -1,5 +1,7 @@
 const check = (req, res, next) => {
-  if (!req.user) { return res.redirect('/user/login')} else { return next() }
+  if (!req.user) { return res.redirect('/user/login')}
+  if (req.user.id == req.query.follow) { res.redirect('back') }
+  else { return next() }
 }
 
 module.exports = {
